e2a866e98cf2cd4f234079912a85ba3fbf84ca8e
vladimir iljin <157532871+vladiljin@users.noreply.github.com>
2024-01-30 00:13:04 +0000

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import kivy
+from kivy.properties import StringProperty
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
@@ -6,6 +8,7 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.metrics import dp
+kivy.require('1.9.0')
 
 class BoxLayoutMain(BoxLayout):
     pass
@@ -13,13 +16,29 @@ class BoxLayoutMain(BoxLayout):
 class OverlayLayout(RelativeLayout):
     pass
 
+'''
 class MyImageButton(Button):
     def __init__(self, **kwargs):
         super(MyImageButton, self).__init__(**kwargs)
         with self.canvas:
             # Add your canvas instructions here if needed
             pass
+'''
 
+class MyImageButton(Button):
+    background_normal = StringProperty('data/images/exit.png')
+    
+    def __init__(self, **kwargs):
+        super(MyImageButton, self).__init__(**kwargs)
+        self.size_hint = (None, None)
+        self.size = ('40dp', '40dp')
+        self.pos_hint = {'x': 0, 'top': 1}
+        self.bind(on_release=self.on_button_release)
+
+    def on_button_release(self, *args):
+        # Handle button release event here
+        pass
+    
 class MyApp(App):
     button_height = dp(38)
     
